4ad0edf9283e2c47e66f1754d2035834442dba28
galiusbro <galiusbro@gmail.com>
2023-02-01 00:37:57 +0800

    Add project files.

---
+++ MyHashTable/HashTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyHashTable
+{
+    class Item<T>
+    {
+        public int Key { get; set; }
+        public List<T> Nodes { get; set; }
+
+        public Item(int key )
+        {
+            Key = key;
+            Nodes = new List<T>();
+        }
+    }
+    internal class HashTable<T>
+    {
+        private Item<T>[] items;
+
+        public HashTable(int size)
+        {
+            items = new Item<T>[size];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new Item<T>(i);
+            }
+        }
+
+        public void Add(T item)
+        {
+            var key = GetHash(item);
+            items[key].Nodes.Add(item);
+        }
+
+        public bool Search(T item)
+        {
+            var key = GetHash(item);
+            return items[key].Nodes.Contains(item);
+        }
+
+        private int GetHash(T item)
+        {
+            return item.GetHashCode() % items.Length;
+        }
+    }
+}
---
+++ MyHashTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyHashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var HashTable = new HashTable<Person>(100);
+            var person = new Person() { Name = "Santa", Age = 18, Gender = 3 };
+            HashTable.Add(new Person() { Name = "Bob", Age = 18, Gender = 0 });
+            HashTable.Add(new Person() { Name = "Elly", Age = 19, Gender = 1 });
+            HashTable.Add(new Person() { Name = "Kate", Age = 18, Gender = 2 });
+            HashTable.Add(person);
+
+            Console.WriteLine(HashTable.Search(person));
+            Console.WriteLine(HashTable.Search(new Person() { Name = "Santa", Age = 18, Gender = 3 }));
+
+            Console.WriteLine();
+        }
+    }
+}
